c83252408aba527320ee1bb8b6c62c285186e21f
Ivan Zhang <ivanzhangemail@gmail.com>
2018-02-17 18:29:24 -0500

    Login and Register View Implementation

--- app/src/main/java/app/izhang/filmfriend/View/LoginView.java
+++ app/src/main/java/app/izhang/filmfriend/View/LoginView.java
@@ -3,6 +3,7 @@ package app.izhang.filmfriend.View;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -64,6 +65,8 @@ public class LoginView extends AppCompatActivity implements LoaderCallbacks<Curs
     private View mProgressView;
     private View mLoginFormView;
 
+    private TextView mRegisterTextBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,6 +97,15 @@ public class LoginView extends AppCompatActivity implements LoaderCallbacks<Curs
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        mRegisterTextBtn = findViewById(R.id.tv_register_textbtn);
+        mRegisterTextBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterView.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void populateAutoComplete() {
--- app/src/main/java/app/izhang/filmfriend/View/RegisterView.java
+++ app/src/main/java/app/izhang/filmfriend/View/RegisterView.java
@@ -1,15 +1,31 @@
 package app.izhang.filmfriend.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import app.izhang.filmfriend.R;
 
 public class RegisterView extends AppCompatActivity {
 
+    // UI references
+    private TextView mLoginTextBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_view);
+
+        mLoginTextBtn = findViewById(R.id.tv_login_textbtn);
+
+        mLoginTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginView.class);
+                startActivity(intent);
+            }
+        });
     }
 }
