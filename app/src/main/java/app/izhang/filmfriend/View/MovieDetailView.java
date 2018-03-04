07a4c9396c53eff65687b8652040ed3e038f56d4
Ivan Zhang <ivanzhangemail@gmail.com>
2018-03-04 14:02:55 -0500

    Coordinated Layout View
    * Added UI to show coordinated layout view on in the MovieDetail View

--- app/src/main/java/app/izhang/filmfriend/View/MovieDetailView.java
+++ app/src/main/java/app/izhang/filmfriend/View/MovieDetailView.java
@@ -2,10 +2,13 @@ package app.izhang.filmfriend.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import app.izhang.filmfriend.R;
 
 public class MovieDetailView extends AppCompatActivity {
+
+    Toolbar mActionBarToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,9 @@ public class MovieDetailView extends AppCompatActivity {
         setContentView(R.layout.activity_movie_detail_view);
 
         // TODO: 3/4/18 Change this to the title of the Movie
-        setTitle("Movie Detail View");
+        mActionBarToolbar = findViewById(R.id.toolbar);
+        mActionBarToolbar.setTitle("Black panther");
+        setSupportActionBar(mActionBarToolbar);
+
     }
 }
