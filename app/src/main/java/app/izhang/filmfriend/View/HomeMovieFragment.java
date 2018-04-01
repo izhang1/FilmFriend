e0c117dc5a9ba0ba0c76b09f5c296b9270b9282d
Ivan Zhang <ivanzhangemail@gmail.com>
2018-04-01 13:47:34 -0400

    Removed Unneeded Firebase Ref

--- app/src/main/java/app/izhang/filmfriend/View/HomeMovieFragment.java
+++ app/src/main/java/app/izhang/filmfriend/View/HomeMovieFragment.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
